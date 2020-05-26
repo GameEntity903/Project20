0df6e8e62e4c2fb3d4ee577d89285911db6350a5
GameEntity903 <65657986+GameEntity903@users.noreply.github.com>
2020-05-26 15:56:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var car,wall;
+var speed,weight;
+var deformation;
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+  car=createSprite(50, 200, 50, 50);
+  wall=createSprite(1500,200,60,canvas.height/2);
+  wall.shapeColor = (80,80,80);
+  car.shapeColor = "white";
+  deformation = (0.5*weight*speed*speed)/22500;
+}
+function draw() {
+  background("black");
+  console.log(deformation); 
+  car.velocityX = speed;  if(car.isTouching(wall)){
+    car.velocityX = 0;
+  }
+  if((deformation<=100)&&(car.isTouching(wall))){
+    car.shapeColor = "green";
+    console.log(deformation);
+     }
+   else{
+    car.shapeColor = "white";   
+     }
+  if((deformation >=100) && (deformation <=180)&&(car.isTouching(wall))){
+      car.shapeColor = "yellow";
+     }
+   else{
+      car.shapeColor = "white";   
+     }
+  if((deformation>=180)&&(car.isTouching(wall))){
+        car.shapeColor = "red";
+     }
+   else{
+        car.shapeColor = "white";   
+     }
+  drawSprites();
+}
